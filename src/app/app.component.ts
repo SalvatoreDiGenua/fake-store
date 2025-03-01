9f1b5063c173ca5a77132a9da43e532b96e46ca8
Salvatore Di Genua <saldigenua@gmail.com>
2025-03-01 16:17:59 +0100

    feat(product-details): enhance product details view with rating, category, and add to cart functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "./shared/components/header/header.component";
 import { RouterOutlet } from '@angular/router';
+import { Toast } from 'primeng/toast';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [RouterOutlet, HeaderComponent]
+  imports: [RouterOutlet, HeaderComponent, Toast],
+  providers: [MessageService]
 })
 export class AppComponent {
 }
--- src/app/features/product-details/product-details.component.ts
+++ src/app/features/product-details/product-details.component.ts
@@ -3,18 +3,24 @@ import { ProductsService } from '../../services/products.service';
 import { ProductImageComponent } from "../../shared/components/product-image/product-image.component";
 import { RatingModule } from 'primeng/rating';
 import { FormsModule } from '@angular/forms';
-import { randomIntFromInterval } from '../../shared/utility/functions';
 import { CurrencyPipe } from '@angular/common';
+import { Button } from 'primeng/button';
+import { Product } from '../../models/product';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-product-details',
-  imports: [ProductImageComponent, RatingModule, FormsModule, CurrencyPipe],
+  imports: [ProductImageComponent, RatingModule, FormsModule, CurrencyPipe, Button],
   templateUrl: './product-details.component.html',
-  styleUrl: './product-details.component.scss'
+  styleUrl: './product-details.component.scss',
 })
 export class ProductDetailsComponent {
   idProduct = input.required<number>();
   #productsService: ProductsService = inject(ProductsService);
   productDetails = this.#productsService.getSingleProduct(this.idProduct);
-  rating = randomIntFromInterval();
+  #messageService: MessageService = inject(MessageService);
+
+  addProductToCart(productDetailsValue: Product) {
+    this.#messageService.add({ severity: 'success', summary: 'Success', detail: `${productDetailsValue.title} added to cart` });
+  }
 }
