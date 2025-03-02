d014f0943323677c446bf22d9e92b0793c9051ec
Salvatore Di Genua <saldigenua@gmail.com>
2025-03-03 00:03:26 +0100

    feat(products): handle empty product state and optimize category selection logic

--- src/app/shared/stores/products/products.selectors.ts
+++ src/app/shared/stores/products/products.selectors.ts
@@ -3,18 +3,17 @@ import { Product } from '../../../models/product';
 import { FakeStoreReducers } from '../app.reducers';
 
 export const getProducts = (state: FakeStoreReducers): Product[] =>
-  state.products;
+  state.products || [];
 
-export const getProductCategories = (state: FakeStoreReducers): string[] => [
-  ...(state.products || []).reduce(
-    (acc, el) => acc.add(el.category),
-    new Set<string>(),
-  ),
-];
+export const getProductCategories = createSelector(getProducts, (products) => [
+  ...products
+    .slice()
+    .reduce((acc, el) => acc.add(el.category), new Set<string>()),
+]);
 
 export const getProductsByCategory = (category: string) =>
   createSelector(getProducts, (products) =>
-    (products || [])
+    products
       .slice()
       .filter((el) => (category ? el.category === category : true)),
   );
