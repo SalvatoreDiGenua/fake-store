85da74ea94a89ba611b115c1fe98a1780fed4020
Salvatore Di Genua <saldigenua@gmail.com>
2025-03-04 12:58:55 +0100

    feat(checkout): enhance checkout component with structured forms and styling

--- src/app/features/checkout/checkout.component.ts
+++ src/app/features/checkout/checkout.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding, ViewEncapsulation } from '@angular/core';
+import { Card } from 'primeng/card';
+import { AccordionModule } from 'primeng/accordion';
 
 @Component({
   selector: 'app-checkout',
-  imports: [],
+  imports: [Card, AccordionModule],
   templateUrl: './checkout.component.html',
-  styleUrl: './checkout.component.scss'
+  styleUrl: './checkout.component.scss',
+  encapsulation: ViewEncapsulation.None,
 })
 export class CheckoutComponent {
-
+  @HostBinding('class') class = 'fake-store-checkout';
 }
