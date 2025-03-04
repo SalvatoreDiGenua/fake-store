5c4c3de013d8c3b05dd8bf250190b042081e3b19
Salvatore Di Genua <saldigenua@gmail.com>
2025-03-04 13:01:50 +0100

    feat(checkout): add back to cart button and improve layout with gap styling

--- src/app/features/checkout/checkout.component.ts
+++ src/app/features/checkout/checkout.component.ts
@@ -1,10 +1,12 @@
 import { Component, HostBinding, ViewEncapsulation } from '@angular/core';
 import { Card } from 'primeng/card';
 import { AccordionModule } from 'primeng/accordion';
+import { Button } from 'primeng/button';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-checkout',
-  imports: [Card, AccordionModule],
+  imports: [Card, AccordionModule, Button, RouterLink],
   templateUrl: './checkout.component.html',
   styleUrl: './checkout.component.scss',
   encapsulation: ViewEncapsulation.None,
