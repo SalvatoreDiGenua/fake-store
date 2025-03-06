bb2953083149da5e081d47d503197ae38a903259
Salvatore Di Genua <saldigenua@gmail.com>
2025-03-06 20:34:55 +0100

    feat(checkout): enhance toolbar with logo and add complete order button

--- src/app/features/checkout/checkout.component.ts
+++ src/app/features/checkout/checkout.component.ts
@@ -1,16 +1,28 @@
-import { Component, HostBinding, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  HostBinding,
+  inject,
+  ViewEncapsulation,
+} from '@angular/core';
 import { Card } from 'primeng/card';
 import { AccordionModule } from 'primeng/accordion';
 import { Button } from 'primeng/button';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { Toolbar } from 'primeng/toolbar';
+import { LogoComponent } from '../../shared/components/logo/logo.component';
 
 @Component({
   selector: 'app-checkout',
-  imports: [Card, AccordionModule, Button, RouterLink],
+  imports: [Card, AccordionModule, Button, RouterLink, Toolbar, LogoComponent],
   templateUrl: './checkout.component.html',
   styleUrl: './checkout.component.scss',
   encapsulation: ViewEncapsulation.None,
 })
 export class CheckoutComponent {
   @HostBinding('class') class = 'fake-store-checkout';
+  #router = inject(Router);
+
+  confirmOrder() {
+    this.#router.navigateByUrl('/order-confirmed');
+  }
 }
