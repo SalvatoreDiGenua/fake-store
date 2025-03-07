5d860af065d68e078a5ed10d0d37a9236a361b35
Salvatore Di Genua <saldigenua@gmail.com>
2025-03-08 00:00:40 +0100

    feat(order-confirmed): update guard logic to redirect users with empty cart to shop products

--- src/app/guards/order-confirmed.guard.ts
+++ src/app/guards/order-confirmed.guard.ts
@@ -3,16 +3,16 @@ import { FakeStoreReducers } from '../shared/stores/app.reducers';
 import { inject } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { getCartCount } from '../shared/stores/cart/cart.selectors';
-import { filter, map, tap } from 'rxjs';
+import { map, tap } from 'rxjs';
 
 export const orderConfirmedGuard: CanActivateFn = () => {
   const store: Store<FakeStoreReducers> = inject(Store<FakeStoreReducers>);
+  const router = inject(Router);
   return store.select(getCartCount).pipe(
-    map((count) => count === 0),
-    filter(Boolean),
-    tap(() => {
-      const router = inject(Router);
-      router.navigateByUrl('/shop');
-    }),
+    map((count) => count > 0),
+    tap(
+      (cartHasProduct) =>
+        !cartHasProduct && router.navigateByUrl('shop/products'),
+    ),
   );
 };
