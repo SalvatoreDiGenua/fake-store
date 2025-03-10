053cfd035764ede67736d45f477d20a0b879bb7e
Salvatore Di Genua <saldigenua@gmail.com>
2025-03-10 12:39:19 +0100

    feat(sign-up): update sign-up form to use email instead of first and last name, add validation and error handling

--- src/app/features/sign-up/sign-up.component.ts
+++ src/app/features/sign-up/sign-up.component.ts
@@ -1,4 +1,10 @@
-import { Component, HostBinding, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  HostBinding,
+  inject,
+  OnDestroy,
+  ViewEncapsulation,
+} from '@angular/core';
 import {
   FormControl,
   FormGroup,
@@ -13,6 +19,10 @@ import { InputTextModule } from 'primeng/inputtext';
 import { PasswordModule } from 'primeng/password';
 import { validateFormGroup } from '../../shared/utility/functions';
 import { RouterLink } from '@angular/router';
+import { MessageService } from 'primeng/api';
+import { Subscription } from 'rxjs';
+import { UserService } from '../../services/user.service';
+import { NewUsersPayload } from '../../models/user';
 
 @Component({
   selector: 'app-sign-up',
@@ -24,23 +34,45 @@ import { RouterLink } from '@angular/router';
     ReactiveFormsModule,
     PasswordModule,
     Button,
-    RouterLink
+    RouterLink,
   ],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.scss',
   encapsulation: ViewEncapsulation.None,
 })
-export class SignUpComponent {
+export class SignUpComponent implements OnDestroy {
   @HostBinding('class') class = 'host-fake-store-signup';
+  #messageService: MessageService = inject(MessageService);
+  #userService: UserService = inject(UserService);
+  #addNewUserSubscription: Subscription = new Subscription();
 
   formSignUp = new FormGroup({
     username: new FormControl('', [Validators.required]),
-    firstname: new FormControl('', [Validators.required]),
-    lastname: new FormControl('', [Validators.required]),
+    email: new FormControl('', [
+      Validators.required,
+      Validators.pattern(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/),
+    ]),
     password: new FormControl('', [Validators.required]),
   });
 
   register() {
     validateFormGroup(this.formSignUp);
+    if (this.formSignUp.invalid) {
+      this.#messageService.add({
+        severity: 'error',
+        summary: 'Error',
+        detail: 'All fields are required!',
+      });
+      return;
+    }
+
+    const newUsersPayload: NewUsersPayload = this.formSignUp.getRawValue();
+    this.#addNewUserSubscription = this.#userService
+      .addNewUser(newUsersPayload)
+      .subscribe();
+  }
+
+  ngOnDestroy() {
+    this.#addNewUserSubscription.unsubscribe();
   }
 }
